f53791bab03e6d82722f55fc1df89d32d27f4042
CoolAlexV <alex.verzea.123@gmail.com>
2017-12-31 16:41:28 -0500

    Fixed the authentication through canLoad post-NgRx.

--- src/app/shared/auth.interceptor.ts
+++ src/app/shared/auth.interceptor.ts
@@ -1,16 +1,21 @@
 import {HttpEvent, HttpHandler, HttpInterceptor, HttpRequest} from "@angular/common/http";
 import { Observable } from 'rxjs/Observable';
 import {Injectable} from "@angular/core";
-import {AuthService} from "../auth/auth.service";
+import {Store} from "@ngrx/store";
+import * as fromApp from '../store/app.reducers';
+import * as fromAuth from '../auth/store/auth.reducers';
+import 'rxjs/add/operator/switchMap';
 
 @Injectable()
 export class AuthInterceptor implements HttpInterceptor {
 
-  constructor(private authService: AuthService) {}
+  constructor(private store: Store<fromApp.AppState>) {}
 
   intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-    const copiedReq = req.clone({params: req.params.set('auth', this.authService.getToken())});
-    console.log(copiedReq);
-    return next.handle(copiedReq);
+    return this.store.select('auth')
+      .switchMap((authState: fromAuth.State) => {
+        const copiedReq = req.clone({params: req.params.set('auth', authState.token)});
+        return next.handle(copiedReq);
+    })
   }
 }
--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -2,35 +2,30 @@ import { Injectable } from '@angular/core';
 import { RecipeService } from "../recipes/recipe.service";
 import { Recipe } from "../recipes/recipe.model";
 import 'rxjs/Rx';
-import {AuthService} from "../auth/auth.service";
 import {HttpClient, HttpParams, HttpRequest} from "@angular/common/http";
 
 @Injectable()
 export class DataStorageService {
+  token = '';
 
   constructor(
     private httpClient: HttpClient,
-    private recipeService: RecipeService,
-    private authService: AuthService) {
+    private recipeService: RecipeService) {
   }
 
   storeRecipes() {
-    const token = this.authService.getToken();
     const req = new HttpRequest(
       'PUT',
       'https://ng-recipe-book-a0d65.firebaseio.com/recipes.json',
       this.recipeService.getRecipes(),
       {
-        params: new HttpParams().set('auth', token),
         reportProgress: true
       });
     return this.httpClient.request(req);
   }
 
   getRecipes() {
-    const token = this.authService.getToken();
-
-    this.httpClient.get<Recipe[]>('https://ng-recipe-book-a0d65.firebaseio.com/recipes.json?auth=' + token, {
+    this.httpClient.get<Recipe[]>('https://ng-recipe-book-a0d65.firebaseio.com/recipes.json', {
       observe: 'body',
       responseType: 'json'
     })
